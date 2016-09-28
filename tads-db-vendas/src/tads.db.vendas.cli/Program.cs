1ad110a77bbe1872b252e2a3eebafe7e672b0c2b
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-09-28 10:34:51 -0300

    adicionado exemplo C# de lista de produtos

---
+++ tads-db-vendas/src/tads.db.vendas.cli/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+using MySql.Data.MySqlClient;
+
+namespace tads.db.vendas.cli {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            var connection = MySqlClientFactory.Instance.CreateConnection();
+
+            connection.ConnectionString =
+                "Server=127.0.0.1;Port=4040;Database=vendas_27_09_2016;Uid=vendas;Pwd=;";
+
+            connection.Open();
+
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = "SELECT * FROM produto";
+            var reader = cmd.ExecuteReader();
+
+            Console.WriteLine("-- Produtos cadastrados --");
+
+            while (reader.Read()) {
+
+                for (var i = 0; i < reader.FieldCount; i++) {
+
+                    Console.WriteLine(
+                        "\t{0}: {1}",
+
+                        reader.GetName(i),
+                        reader.GetValue(i));
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
