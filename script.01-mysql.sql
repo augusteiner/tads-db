ae118bd95bd3d5d03b1d3b5f47ba46dd1c534536
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-10-19 20:06:48 -0300

    port MySQL da estrutura da base de exemplo

--- script.01-mysql.sql
+++ script.01-mysql.sql
@@ -4,319 +4,365 @@
 /****************************************************************************/
 
 CREATE DATABASE Exemplo
-GO
+
+;
 
 /****************************************************************************/
 /* Estabelecendo uma Conexão com o Database Exemplo                         */
 /****************************************************************************/
 
-USE Exemplo
-GO
+USE Exemplo;
 
 /****************************************************************************/
-/* Criando as tabelas do database SYSAMPLES                                 */
+/* Criando AS tabelas do database SYSAMPLES                                 */
 /****************************************************************************/
 
 CREATE TABLE TipoEnd (
-  Cod_TipoEnd int identity not null,
-  Nome_TipoEnd varchar(30) not null,
 
-  Constraint PK_TipoEnd Primary Key(Cod_TipoEnd),
+  Cod_TipoEnd INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Nome_TipoEnd VARCHAR(30) NOT NULL,
 
-  Constraint UQ_TipoEnd Unique(Nome_TipoEnd)
+  CONSTRAINT PK_TipoEnd PRIMARY KEY (Cod_TipoEnd),
+
+  CONSTRAINT UQ_TipoEnd UNIQUE KEY (Nome_TipoEnd)
+
 )
-GO
+;
 
 CREATE TABLE Estado (
-  Sigla_Est char(02) not null,
-  Nome_Est varchar(100) not null,
 
-  Constraint PK_Est Primary Key(Sigla_Est),
+  Sigla_Est CHAR(02) NOT NULL,
+  Nome_Est VARCHAR(100) NOT NULL,
 
-  Constraint UQ_Est Unique(Nome_Est)
+  CONSTRAINT PK_Est PRIMARY KEY (Sigla_Est),
+
+  CONSTRAINT UQ_Est UNIQUE KEY (Nome_Est)
+
 )
-GO
+;
 
 CREATE TABLE Cidade (
-  Cod_Cid int Identity Not Null,
-  Sigla_Est char(02) Not Null,
-  Nome_Cid varchar(100) Not Null,
 
-  Constraint PK_Cid Primary Key(Cod_Cid),
+  Cod_Cid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Sigla_Est CHAR(02) NOT NULL,
+  Nome_Cid VARCHAR(100) NOT NULL,
 
-  Constraint FK_Cid Foreign Key(Sigla_Est) References Estado(Sigla_Est),
+  CONSTRAINT PK_Cid PRIMARY KEY (Cod_Cid),
 
-  Constraint UQ_Cid Unique(Sigla_Est,Nome_Cid)
+  CONSTRAINT FK_Cid FOREIGN KEY (Sigla_Est) REFERENCES Estado (Sigla_Est),
+
+  CONSTRAINT UQ_Cid UNIQUE KEY (Sigla_Est, Nome_Cid)
+
 )
-GO
+;
 
 CREATE TABLE TipoCli (
-  Cod_TipoCli int identity not null,
-  Nome_TipoCli varchar(100) not null,
 
-  Constraint PK_TipoCli Primary key(Cod_TipoCli),
+  Cod_TipoCli INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Nome_TipoCli VARCHAR(100) NOT NULL,
 
-  Constraint UQ_TipoCli Unique(Nome_TipoCli)
+  CONSTRAINT PK_TipoCli PRIMARY KEY (Cod_TipoCli),
+
+  CONSTRAINT UQ_TipoCli UNIQUE KEY (Nome_TipoCli)
+
 )
-GO
+;
 
 CREATE TABLE Cliente (
-  Cod_Cli int Identity not null,
-  Cod_TipoCli int not null,
-  Nome_Cli varchar(100) not null,
-  Data_CadCli smalldatetime not null Default Getdate(),
-  Renda_Cli decimal(10,2) not null Default 0,
-  Sexo_Cli char(01) not null Default 'F',
 
-  Constraint PK_Cli Primary Key(Cod_Cli),
+  Cod_Cli INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_TipoCli INT UNSIGNED NOT NULL,
+  Nome_Cli VARCHAR(100) NOT NULL,
+  Data_CadCli DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  Renda_Cli DECIMAL(10,2) NOT NULL DEFAULT 0,
+  Sexo_Cli CHAR(01) NOT NULL DEFAULT 'F',
 
-  Constraint FK_Cli Foreign Key(Cod_TipoCli)References TipoCli(Cod_TipoCli),
+  CONSTRAINT PK_Cli PRIMARY KEY (Cod_Cli),
 
-  Constraint CH_Cli1 Check(Renda_Cli >=0),
-  Constraint CH_Cli2 Check(Sexo_Cli IN('F','M'))
+  CONSTRAINT FK_Cli FOREIGN KEY (Cod_TipoCli) REFERENCES TipoCli (Cod_TipoCli),
+
+  CONSTRAINT CH_Cli1 CHECK (Renda_Cli >= 0),
+  CONSTRAINT CH_Cli2 CHECK (Sexo_Cli IN ('F', 'M'))
+
 )
-GO
+;
 
 CREATE TABLE Conjuge (
-  Cod_Cli int not null,
-  Nome_Conj char(30) not null,
-  Renda_Conj decimal(10,2) not null Default 0,
-  Sexo_Conj char(01) not null Default 'M',
 
-  Constraint PK_Conj Primary Key(Cod_Cli),
+  Cod_Cli INT UNSIGNED NOT NULL,
+  Nome_Conj CHAR(30) NOT NULL,
+  Renda_Conj DECIMAL(10,2) NOT NULL DEFAULT 0,
+  Sexo_Conj CHAR(01) NOT NULL DEFAULT 'M',
 
-  Constraint FK_Conj Foreign Key(Cod_Cli) References Cliente(Cod_Cli),
+  CONSTRAINT PK_Conj PRIMARY KEY (Cod_Cli),
 
-  Constraint CH_Conj1 Check(Renda_Conj >=0),
-  Constraint CH_Conj2 Check(Sexo_Conj IN ('F','M'))
+  CONSTRAINT FK_Conj FOREIGN KEY (Cod_Cli) REFERENCES Cliente (Cod_Cli),
+
+  CONSTRAINT CH_Conj1 CHECK (Renda_Conj >= 0),
+  CONSTRAINT CH_Conj2 CHECK (Sexo_Conj IN ('F', 'M'))
+
 )
-GO
+;
 
 CREATE TABLE Endereco (
-  Cod_End int identity not null,
-  Cod_TipoEnd int not null,
-  Cod_Cid int not null,
-  Cod_Cli int not null,
-  Nome_Rua varchar(100) not null,
-  Nome_Bairro varchar(100) not null,
-  Compl_End varchar(100) null,
 
-  Constraint PK_End Primary Key(Cod_End),
+  Cod_End INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_TipoEnd INT UNSIGNED NOT NULL,
+  Cod_Cid INT UNSIGNED NOT NULL,
+  Cod_Cli INT UNSIGNED NOT NULL,
+  Nome_Rua VARCHAR(100) NOT NULL,
+  Nome_Bairro VARCHAR(100) NOT NULL,
+  Compl_End VARCHAR(100) NULL,
 
-  Constraint FK_End1 Foreign Key(Cod_TipoEnd) References TipoEnd(Cod_TipoEnd),
-  Constraint FK_End2 Foreign Key(Cod_Cid) References Cidade(Cod_Cid),
-  Constraint FK_End3 Foreign Key(Cod_Cli) References Cliente(Cod_Cli)
+  CONSTRAINT PK_End PRIMARY KEY (Cod_End),
+
+  CONSTRAINT FK_End1 FOREIGN KEY (Cod_TipoEnd) REFERENCES TipoEnd (Cod_TipoEnd),
+  CONSTRAINT FK_End2 FOREIGN KEY (Cod_Cid) REFERENCES Cidade (Cod_Cid),
+  CONSTRAINT FK_End3 FOREIGN KEY (Cod_Cli) REFERENCES Cliente (Cod_Cli)
+
 )
-GO
+;
 
 CREATE TABLE Credito (
-  Num_Lanc int identity not null,
-  Cod_Cli int not null,
-  Cred_Cli decimal(10,2) not null,
-  Data_CredCli smalldatetime not null,
 
-  Constraint PK_Cred Primary Key(Num_Lanc),
+  Num_Lanc INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_Cli INT UNSIGNED NOT NULL,
+  Cred_Cli DECIMAL(10,2) NOT NULL,
+  Data_CredCli DATETIME NOT NULL,
 
-  Constraint FK_Cred Foreign Key(Cod_Cli) References Cliente(Cod_Cli),
+  CONSTRAINT PK_Cred PRIMARY KEY (Num_Lanc),
 
-  Constraint CH_Cred Check(Cred_Cli > 0)
+  CONSTRAINT FK_Cred FOREIGN KEY (Cod_Cli) REFERENCES Cliente (Cod_Cli),
+
+  CONSTRAINT CH_Cred CHECK (Cred_Cli > 0)
+
 )
-GO
+;
 
 CREATE TABLE Fone (
-  Num_Lanc int identity not null,
-  Cod_Cli int not null,
-  Num_Fone char(10) not null,
-  Num_DDD char(05) not null Default '011',
 
-  Constraint PK_Fone Primary Key(Num_Lanc),
+  Num_Lanc INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_Cli INT UNSIGNED NOT NULL,
+  Num_Fone CHAR(10) NOT NULL,
+  Num_DDD CHAR(05) NOT NULL DEFAULT '011',
 
-  Constraint FK_Fone Foreign Key(Cod_Cli) References Cliente(Cod_Cli)
+  CONSTRAINT PK_Fone PRIMARY KEY (Num_Lanc),
+
+  CONSTRAINT FK_Fone FOREIGN KEY (Cod_Cli) REFERENCES Cliente (Cod_Cli)
+
 )
-GO
+;
 
 CREATE TABLE EMail (
-  Num_Lanc int identity not null,
-  Cod_Cli int not null,
-  EMail_Cli varchar(255) not null,
 
-  Constraint PK_Email Primary Key(Num_Lanc),
+  Num_Lanc INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_Cli INT UNSIGNED NOT NULL,
+  EMail_Cli VARCHAR(255) NOT NULL,
 
-  Constraint FK_Emails Foreign Key(Cod_Cli) References Cliente(Cod_Cli)
+  CONSTRAINT PK_Email PRIMARY KEY (Num_Lanc),
+
+  CONSTRAINT FK_Emails FOREIGN KEY (Cod_Cli) REFERENCES Cliente (Cod_Cli)
+
 )
-GO
+;
 
 CREATE TABLE StatusPedido (
-  Cod_Sta smallint identity not null,
-  Sta_Ped varchar(100) not null,
 
-  Constraint PK_StatusPed Primary Key(Cod_Sta),
+  Cod_Sta SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Sta_Ped VARCHAR(100) NOT NULL,
 
-  Constraint UQ_StatusPed Unique(Sta_Ped)
+  CONSTRAINT PK_StatusPed PRIMARY KEY (Cod_Sta),
+
+  CONSTRAINT UQ_StatusPed UNIQUE KEY (Sta_Ped)
+
 )
-GO
+;
 
 CREATE TABLE Funcionario (
-  Cod_Func int Identity not null,
-  Nome_Func varchar(100) not null,
-  Data_CadFunc smalldatetime not null Default Getdate(),
-  Sexo_Func char(01) not null Default 'F',
-  Sal_Func decimal(10,2) not null Default 200,
-  End_Func varchar(100) not null,
 
-  Constraint PK_Func Primary Key(Cod_Func),
+  Cod_Func INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Nome_Func VARCHAR(100) NOT NULL,
+  Data_CadFunc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  Sexo_Func CHAR(01) NOT NULL DEFAULT 'F',
+  Sal_Func DECIMAL(10,2) NOT NULL DEFAULT 200,
+  End_Func VARCHAR(100) NOT NULL,
 
-  Constraint CH_Func1 Check(Data_CadFunc >=Getdate()),
-  Constraint CH_Func2 Check(Sexo_Func IN ('F','M')),
-  Constraint CH_Func3 Check(Sal_Func >=0)
+  CONSTRAINT PK_Func PRIMARY KEY (Cod_Func),
+
+  CONSTRAINT CH_Func1 CHECK (Data_CadFunc >= CURDATE()),
+  CONSTRAINT CH_Func2 CHECK (Sexo_Func IN ('F', 'M')),
+  CONSTRAINT CH_Func3 CHECK (Sal_Func >= 0)
+
 )
-GO
+;
 
 CREATE TABLE Bonus (
-  Num_Lanc int identity not null,
-  Cod_Func int not null,
-  Data_Bonus smalldatetime not null default Getdate(),
-  Val_Bonus decimal(10,2) not null,
 
-  Constraint PK_Bonus Primary Key(Num_Lanc),
+  Num_Lanc INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_Func INT UNSIGNED NOT NULL,
+  Data_Bonus DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  Val_Bonus DECIMAL(10,2) NOT NULL,
 
-  Constraint FK_Bonus Foreign Key(Cod_Func) References Funcionario(Cod_Func),
+  CONSTRAINT PK_Bonus PRIMARY KEY (Num_Lanc),
 
-  Constraint CH_Bonus1 Check(Data_Bonus >= Getdate()),
-  Constraint CH_Bonus2 Check(Val_Bonus > 0)
+  CONSTRAINT FK_Bonus FOREIGN KEY (Cod_Func) REFERENCES Funcionario (Cod_Func),
+
+  CONSTRAINT CH_Bonus1 CHECK (Data_Bonus >= CURDATE()),
+  CONSTRAINT CH_Bonus2 CHECK (Val_Bonus > 0)
+
 )
-GO
+;
 
 CREATE TABLE Pontuacao (
-  Num_Lanc int identity not null,
-  Cod_Func int not null,
-  Data_Pto smalldatetime not null default Getdate(),
-  Pto_Func decimal(4,2) not null,
 
-  Constraint PK_Pto Primary Key(Num_Lanc),
+  Num_Lanc INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_Func INT UNSIGNED NOT NULL,
+  Data_Pto DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  Pto_Func DECIMAL(4,2) NOT NULL,
 
-  Constraint FK_Pto Foreign Key(Cod_Func) References Funcionario(Cod_Func),
+  CONSTRAINT PK_Pto PRIMARY KEY (Num_Lanc),
 
-  Constraint CH_Pto1 Check(Data_Pto >= Getdate()),
-  Constraint CH_Pto2 Check(Pto_Func > 0)
+  CONSTRAINT FK_Pto FOREIGN KEY (Cod_Func) REFERENCES Funcionario (Cod_Func),
+
+  CONSTRAINT CH_Pto1 CHECK (Data_Pto >= CURDATE()),
+  CONSTRAINT CH_Pto2 CHECK (Pto_Func > 0)
+
 )
-GO
+;
 
 CREATE TABLE Historico (
-  Num_Lanc int identity not null,
-  Cod_Func int not null,
-  Data_Hist smalldatetime not null Default Getdate(),
-  Sal_Ant decimal(10,2) not null,
-  Sal_Atual decimal(10,2) not null,
 
-  Constraint PK_Hist Primary Key(Num_Lanc),
+  Num_Lanc INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_Func INT UNSIGNED NOT NULL,
+  Data_Hist DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  Sal_Ant DECIMAL(10,2) NOT NULL,
+  Sal_Atual DECIMAL(10,2) NOT NULL,
 
-  Constraint FK_Hist Foreign Key(Cod_Func) References Funcionario(Cod_Func),
+  CONSTRAINT PK_Hist PRIMARY KEY (Num_Lanc),
 
-  Constraint CH_Hist1 Check(Data_Hist >= Getdate()),
-  Constraint CH_Hist2 Check(Sal_Ant >= 0),
-  Constraint CH_Hist3 Check(Sal_Ant > 0)
+  CONSTRAINT FK_Hist FOREIGN KEY (Cod_Func) REFERENCES Funcionario (Cod_Func),
+
+  CONSTRAINT CH_Hist1 CHECK (Data_Hist >= CURDATE()),
+  CONSTRAINT CH_Hist2 CHECK (Sal_Ant >= 0),
+  CONSTRAINT CH_Hist3 CHECK (Sal_Ant > 0)
+
 )
-GO
+;
 
 CREATE TABLE Dependente (
-  Cod_Dep int identity not null,
-  Cod_Func int not null,
-  Nome_Dep varchar(100) not null,
-  Data_NascDep smalldatetime not null,
-  Sexo_Dep char(01) not null Default 'F',
 
-  Constraint PK_Dep Primary Key(Cod_Dep),
+  Cod_Dep INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_Func INT UNSIGNED NOT NULL,
+  Nome_Dep VARCHAR(100) NOT NULL,
+  Data_NascDep DATETIME NOT NULL,
+  Sexo_Dep CHAR(01) NOT NULL DEFAULT 'F',
 
-  Constraint FK_Dep Foreign Key(Cod_Func) References Funcionario(Cod_Func),
+  CONSTRAINT PK_Dep PRIMARY KEY (Cod_Dep),
 
-  Constraint CH_Dep Check(Sexo_Dep IN ('F','M'))
+  CONSTRAINT FK_Dep FOREIGN KEY (Cod_Func) REFERENCES Funcionario (Cod_Func),
+
+  CONSTRAINT CH_Dep CHECK (Sexo_Dep IN ('F', 'M'))
+
 )
-GO
+;
 
 CREATE TABLE Pedido (
-  Num_Ped int identity not null,
-  Cod_Cli int not null,
-  Cod_Func int not null,
-  Cod_Sta smallint not null,
-  Data_Ped smalldatetime not null Default Getdate(),
-  Val_Ped decimal(10,2) not null Default 0,
 
-  Constraint PK_Pedido Primary Key(Num_Ped),
+  Num_Ped INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_Cli INT UNSIGNED NOT NULL,
+  Cod_Func INT UNSIGNED NOT NULL,
+  Cod_Sta SMALLINT UNSIGNED NOT NULL,
+  Data_Ped DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  Val_Ped DECIMAL(10,2) NOT NULL DEFAULT 0,
 
-  Constraint FK_Pedido1 Foreign Key(Cod_Cli) References Cliente(Cod_Cli),
-  Constraint FK_Pedido2 Foreign Key(Cod_Cli) References Funcionario(Cod_Func),
+  CONSTRAINT PK_Pedido PRIMARY KEY (Num_Ped),
 
-  Constraint CH_Pedido1 Check(Data_Ped >=getdate()),
-  Constraint CH_Pedido2 Check(Val_Ped >=0)
+  CONSTRAINT FK_Pedido1 FOREIGN KEY (Cod_Cli) REFERENCES Cliente (Cod_Cli),
+  CONSTRAINT FK_Pedido2 FOREIGN KEY (Cod_Cli) REFERENCES Funcionario (Cod_Func),
+
+  CONSTRAINT CH_Pedido1 CHECK (Data_Ped >= CURDATE()),
+  CONSTRAINT CH_Pedido2 CHECK (Val_Ped >=0)
+
 )
-GO
+;
 
 CREATE TABLE Parcela (
-  Num_Par smallint not null,
-  Num_Ped int not null,
-  Data_Venc smalldatetime not null Default getdate(),
-  Val_Venc decimal(10,2) not null,
-  Data_Pgto smalldatetime null,
-  Val_Pgto as CASE
-    WHEN Data_Pgto < Data_Venc THEN Val_Venc * 0.9
-    WHEN Data_Pgto = Data_Venc THEN Val_Venc
-    WHEN Data_Pgto > Data_Venc THEN Val_Venc * 1.1
-  END,
 
-  Constraint PK_Parcela Primary key(Num_Par,Num_Ped),
+  Num_Par SMALLINT UNSIGNED NOT NULL,
+  Num_Ped INT UNSIGNED NOT NULL,
+  Data_Venc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  Val_Venc DECIMAL(10,2) NOT NULL,
+  Data_Pgto DATETIME NULL,
+  Val_Pgto DECIMAL(10,2) AS (
+    CASE
+      WHEN Data_Pgto < Data_Venc THEN
+        Val_Venc * 0.9
+      WHEN Data_Pgto = Data_Venc THEN
+        Val_Venc
+      WHEN Data_Pgto > Data_Venc THEN
+        Val_Venc * 1.1
+    END),
 
-  Constraint FK_Parcela Foreign Key(Num_Ped)References Pedido(Num_Ped),
+  CONSTRAINT PK_Parcela PRIMARY KEY (Num_Par,Num_Ped),
 
-  Constraint CH_Parcela1 Check(Data_Venc >= getdate()),
-  Constraint CH_Parcela2 Check(Val_Venc >= 0),
+  CONSTRAINT FK_Parcela FOREIGN KEY (Num_Ped) REFERENCES Pedido (Num_Ped),
+
+  CONSTRAINT CH_Parcela1 CHECK (Data_Venc >= CURDATE()),
+  CONSTRAINT CH_Parcela2 CHECK (Val_Venc >= 0)
+
 )
-GO
+;
 
 CREATE TABLE TipoProd (
-  Cod_TipoProd int identity not null,
-  Nome_TipoProd varchar(100) not null,
 
-  Constraint PK_TipoProd Primary Key(Cod_TipoProd),
+  Cod_TipoProd INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Nome_TipoProd VARCHAR(100) NOT NULL,
 
-  Constraint UQ_TipoProd Unique(Nome_TipoProd)
+  CONSTRAINT PK_TipoProd PRIMARY KEY (Cod_TipoProd),
+
+  CONSTRAINT UQ_TipoProd UNIQUE KEY (Nome_TipoProd)
+
 )
-GO
+;
 
 CREATE TABLE Produto (
-  Cod_Prod int identity not null,
-  Cod_TipoProd int not null,
-  Nome_Prod varchar(100) not null,
-  Qtd_EstqProd int not null Default 0,
-  Val_UnitProd decimal(10,2) not null,
-  Val_Total AS Qtd_EstqProd * Val_UnitProd,
 
-  Constraint PK_Prod Primary Key(Cod_Prod),
+  Cod_Prod INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  Cod_TipoProd INT UNSIGNED NOT NULL,
+  Nome_Prod VARCHAR(100) NOT NULL,
+  Qtd_EstqProd INT UNSIGNED NOT NULL DEFAULT 0,
+  Val_UnitProd DECIMAL(10,2) NOT NULL Check (Val_UnitProd > 0),
+  Val_Total DECIMAL(10,2) AS (Qtd_EstqProd * Val_UnitProd),
 
-  Constraint FK_Prod Foreign Key(Cod_TipoProd) References TipoProd(Cod_TipoProd),
+  CONSTRAINT PK_Prod PRIMARY KEY (Cod_Prod),
 
-  Constraint UQ_Prod Unique(Nome_Prod),
+  CONSTRAINT FK_Prod FOREIGN KEY (Cod_TipoProd) REFERENCES TipoProd (Cod_TipoProd),
 
-  Constraint CH_Prod1 Check(Qtd_EstqProd >= 0),
-  Constraint CH_Prod2 Check(Val_UnitProd > 0)
+  CONSTRAINT UQ_Prod UNIQUE KEY (Nome_Prod),
+
+  CONSTRAINT CH_Prod1 CHECK (Qtd_EstqProd >= 0),
+  CONSTRAINT CH_Prod2 CHECK (Val_UnitProd > 0)
+
 )
-GO
+;
 
 CREATE TABLE Itens (
-  Num_Ped int not null,
-  Cod_Prod int not null,
-  Qtd_Vend int not null,
-  Val_Vend decimal(10,2) not null,
 
-  Constraint PK_Itens Primary Key(Num_Ped,Cod_Prod),
+  Num_Ped INT UNSIGNED NOT NULL,
+  Cod_Prod INT UNSIGNED NOT NULL,
+  Qtd_Vend INT UNSIGNED NOT NULL,
+  Val_Vend DECIMAL(10,2) NOT NULL,
 
-  Constraint FK_Itens1 Foreign Key(Num_Ped) References Pedido(Num_Ped),
-  Constraint FK_Itens2 Foreign Key(Cod_Prod) References Produto(Cod_Prod),
+  CONSTRAINT PK_Itens PRIMARY KEY (Num_Ped,Cod_Prod),
 
-  Constraint CH_Itens1 Check(Qtd_Vend > 0),
-  Constraint CH_Itens2 Check(Val_Vend > 0)
+  CONSTRAINT FK_Itens1 FOREIGN KEY (Num_Ped) REFERENCES Pedido (Num_Ped),
+  CONSTRAINT FK_Itens2 FOREIGN KEY (Cod_Prod) REFERENCES Produto (Cod_Prod),
+
+  CONSTRAINT CH_Itens1 CHECK (Qtd_Vend > 0),
+  CONSTRAINT CH_Itens2 CHECK (Val_Vend > 0)
+
 )
-GO
+;
 
 /****************************************************************************/
 /* Verificando a Criação da Tabelas do Database SYSAMPLES                   */
@@ -325,5 +371,7 @@ GO
 SELECT *
 FROM Information_Schema.Tables
 WHERE Table_Type = 'Base Table'
+
+;
 
 /****************************************************************************/
