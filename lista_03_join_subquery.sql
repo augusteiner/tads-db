8a99fd6d9ab995f4e21851e8ab9cefacb369cae8
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2017-02-02 12:12:48 -0300

    finalizada lista 3 de join + subquery

--- lista_03_join_subquery.sql
+++ lista_03_join_subquery.sql
@@ -13,6 +13,15 @@ ON c.Cod_Cli = p.Cod_Cli
 INNER JOIN Funcionario AS f
 ON f.Cod_Func = p.Cod_Func;
 
+-- OU
+
+SELECT p.Num_Ped, p.Data_Ped, p.Val_Ped,
+       ( SELECT Nome_Cli FROM Cliente WHERE Cod_Cli = p.Cod_Cli ) AS Nome_Cli,
+       ( SELECT Nome_Func FROM Funcionario WHERE Cod_Func = p.Cod_Func ) AS Nome_Func
+
+FROM Pedido AS p;
+
+
 -- 2. Escreva um comando que apresente o nome de cada cliente e o nome do cônjuge
 --    daqueles que forem casados.
 
@@ -22,6 +31,12 @@ FROM Cliente AS ci
 
 LEFT JOIN Conjuge AS co
 ON ci.Cod_Cli = co.Cod_Cli;
+
+-- OU
+
+SELECT Nome_Cli, ( SELECT Nome_Conj FROM Conjuge WHERE Conjuge.Cod_Cli = c.Cod_Cli ) Nome_Conj
+
+FROM Cliente AS c;
 
 -- 3. Escreva um comando que apresente a renda de cada cliente, juntando a renda do cliente
 --    e do cônjuge para os clientes casados.
@@ -33,18 +48,76 @@ FROM Cliente
 LEFT JOIN Conjuge
 ON Conjuge.Cod_Cli = Cliente.Cod_Cli;
 
+-- OU
+
+SELECT c.Nome_Cli,
+       c.Renda_Cli + COALESCE(( SELECT Renda_Conj FROM Conjuge WHERE Conjuge.Cod_Cli = c.Cod_Cli ), 0) Renda_Somada
+
+FROM Cliente c;
+
+
 -- 4. Escreva um comando que apresente os dados dos clientes, os e-mails de cada um e os
 --    telefones de cada um.
 
-SELECT c.Nome_Cli, c.Renda_Cli, c.Sexo_Cli, f.Num_fone, e.Email_Cli
+SELECT c.*, f.Num_Fone Contato, 'FONE' AS Contato_Tipo
 
 FROM Cliente AS c
 
-LEFT JOIN Fone AS f
-ON c.Cod_Cli = f.Cod_Cli
+INNER JOIN Fone AS f
+ON f.Cod_Cli = c.Cod_Cli
 
-LEFT JOIN Email AS e
-ON c.Cod_Cli = e.Cod_Cli;
+UNION
+
+SELECT c.*, e.EMail_Cli Contato, 'EMAIL' AS Contato_Tipo
+
+FROM Cliente AS c
+
+INNER JOIN EMail e
+ON e.Cod_Cli = c.Cod_Cli;
+
+-- OU
+
+SELECT c.*, f.Num_Fone Contato, 'FONE' AS Contato_Tipo
+
+FROM Cliente AS c, Fone f
+
+WHERE c.Cod_Cli IN (f.Cod_Cli)
+
+UNION
+
+SELECT c.*, e.EMail_Cli Contato, 'EMAIL' AS Contato_Tipo
+
+FROM Cliente AS c, EMail e
+
+WHERE c.Cod_Cli IN (e.Cod_Cli);
+
+-- OU (Spartan Mode) :: Somente subqueries
+
+SELECT
+  ( SELECT Cod_Cli FROM Cliente WHERE Cod_Cli = f.Cod_Cli) Cod_Cli,
+  ( SELECT Cod_TipoCli FROM Cliente WHERE Cod_Cli = f.Cod_Cli) Cod_TipoCli,
+  ( SELECT Nome_Cli FROM Cliente WHERE Cod_Cli = f.Cod_Cli) Nome_Cli,
+  ( SELECT Data_CadCli FROM Cliente WHERE Cod_Cli = f.Cod_Cli) Data_CadCli,
+  ( SELECT Renda_Cli FROM Cliente WHERE Cod_Cli = f.Cod_Cli) Renda_Cli,
+  ( SELECT Sexo_Cli FROM Cliente WHERE Cod_Cli = f.Cod_Cli) Sexo_Cli,
+  f.Num_Fone AS Contato,
+  'FONE' AS Contato_Tipo
+
+FROM Fone f
+
+UNION
+
+SELECT
+  ( SELECT Cod_Cli FROM Cliente WHERE Cod_Cli = e.Cod_Cli) Cod_Cli,
+  ( SELECT Cod_TipoCli FROM Cliente WHERE Cod_Cli = e.Cod_Cli) Cod_TipoCli,
+  ( SELECT Nome_Cli FROM Cliente WHERE Cod_Cli = e.Cod_Cli) Nome_Cli,
+  ( SELECT Data_CadCli FROM Cliente WHERE Cod_Cli = e.Cod_Cli) Data_CadCli,
+  ( SELECT Renda_Cli FROM Cliente WHERE Cod_Cli = e.Cod_Cli) Renda_Cli,
+  ( SELECT Sexo_Cli FROM Cliente WHERE Cod_Cli = e.Cod_Cli) Sexo_Cli,
+  e.EMail_Cli AS Contato,
+  'EMAIL' AS Contato_Tipo
+
+FROM EMail e;
 
 -- 5. Escreva um comando que mostre os nomes dos clientes, os endereços com o nome de cada cidade
 --    e o nome de cada estado de cada endereço que cada cliente tem.
@@ -62,15 +135,35 @@ ON ci.Cod_Cid = e.Cod_Cid
 LEFT JOIN Estado AS es
 ON es.Sigla_Est = ci.Sigla_Est;
 
+-- OU
+
+SELECT
+  ( SELECT Nome_Cli FROM Cliente WHERE Cod_Cli = e.Cod_Cli ) AS Nome_Cli,
+  e.Nome_Rua, e.Nome_Bairro, e.Compl_End,
+  ( SELECT Nome_Cid FROM Cidade WHERE Cod_Cid = e.Cod_Cid ) AS Nome_Cid,
+  ( SELECT Nome_Est FROM Estado WHERE Sigla_Est IN ( SELECT Sigla_Est FROM Cidade WHERE Cod_Cid = e.Cod_Cid )) AS Nome_Est
+
+FROM Endereco AS e;
+
 -- 6. Escreva um comando que mostre o nome de cada funcionário e o nome de cada dependente
 --    de que cada funcionário que tem dependentes.
 
-SELECT f.Nome_Func, d.Nome_Dep
+SELECT f.Cod_Func, f.Nome_Func, d.Nome_Dep
 
 FROM Funcionario AS f
 
 INNER JOIN Dependente AS d
 ON f.Cod_Func = d.Cod_Func;
+
+-- OU
+
+SELECT
+  ( SELECT Cod_Func FROM Funcionario WHERE Cod_Func = d.Cod_Func ) Cod_Func,
+  ( SELECT Nome_Func FROM Funcionario WHERE Cod_Func = d.Cod_Func ) Nome_Func,
+  d.Nome_Dep
+
+FROM Dependente AS d;
+
 
 -- 7. Escreva um comando que apresente o nome dos funcionários e o histórico de cada um.
 
@@ -80,6 +173,15 @@ FROM Funcionario as f
 
 LEFT JOIN Historico as h
 ON f.Cod_Func = h.Cod_Func;
+
+-- OU
+
+SELECT
+  h.Cod_Func,
+  ( SELECT f.Nome_Func FROM Funcionario f WHERE f.Cod_Func = h.Cod_Func ),
+  h.Sal_Ant, h.Sal_Atual
+
+FROM Historico as h;
 
 -- 8. Escreva um comando que mostre o nome de cada cliente, o número, data e valor total de cada pedido
 --    que cada cliente fez e o valor de cada parcela de cada pedido.
@@ -93,6 +195,19 @@ ON c.Cod_Cli = p.Cod_Cli
 
 INNER JOIN Parcela AS pa
 ON p.Num_Ped = pa.Num_Ped;
+
+-- OU
+
+SELECT
+  ( SELECT Nome_Cli FROM Cliente WHERE Cod_Cli IN ( SELECT Cod_Cli FROM Pedido WHERE Num_Ped = p.Num_Ped ) ) Nome_Cli,
+
+  ( SELECT Num_Ped FROM Pedido WHERE Num_Ped = p.Num_Ped ) Num_Ped,
+  ( SELECT Data_Ped FROM Pedido WHERE Num_Ped = p.Num_Ped ) Data_Ped,
+  ( SELECT Val_Ped FROM Pedido WHERE Num_Ped = p.Num_Ped ) Val_Ped,
+
+  p.Num_Par, p.Val_Venc
+
+FROM Parcela AS p;
 
 -- 9. Escreva um comando que apresente um relatório de todos os funcionário e clientes que você tem no banco Exemplo.
 
@@ -170,7 +285,7 @@ WHERE NOT EXISTS ( SELECT c.Cod_Cli FROM Fone f WHERE f.Cod_Cli = c.Cod_Cli );
 
 -- 13. Escreva um comando que apresente os dados dos clientes que não têm telefone nem emails.
 
-SELECT c.*
+SELECT DISTINCT c.*
 
 FROM Cliente AS c
 
@@ -180,9 +295,9 @@ ON f.Cod_Cli = c.Cod_Cli
 LEFT JOIN Email AS e
 ON e.Cod_Cli = c.Cod_Cli
 
-WHERE f.Cod_Cli IS NULL AND e.Cod_Cli IS NULL
-
-GROUP BY c.Cod_Cli;
+WHERE
+  f.Cod_Cli IS NULL
+  AND e.Cod_Cli IS NULL;
 
 -- OU
 
@@ -196,7 +311,7 @@ WHERE
 
 -- 14. Escreva um comando que apresente os dados de todos os clientes que não fizeram nenhum pedido.
 
-SELECT c.*
+SELECT DISTINCT c.*
 
 FROM Cliente AS c
 
@@ -207,7 +322,7 @@ WHERE p.Cod_Cli IS NULL;
 
 -- OU
 
-SELECT c.*
+SELECT c.Cod_Cli, c.Cod_TipoCli, c.Nome_Cli, c.Data_CadCli, c.Renda_Cli, c.Sexo_Cli
 
 FROM Cliente AS c
 
@@ -215,14 +330,12 @@ WHERE c.Cod_Cli NOT IN ( SELECT Cod_Cli FROM Pedido AS p WHERE p.Cod_Cli = c.Cod
 
 -- 15. Escreva um comando que apresente os dados dos clientes que fizeram pelo menos um pedido.
 
-SELECT c.*
+SELECT DISTINCT c.*
 
 FROM Cliente AS c
 
 INNER JOIN Pedido AS p
-ON p.Cod_Cli = c.Cod_Cli
-
-GROUP BY c.Cod_Cli;
+ON p.Cod_Cli = c.Cod_Cli;
 
 -- OU
 
@@ -235,16 +348,14 @@ WHERE c.Cod_Cli IN ( SELECT Cod_Cli FROM Pedido );
 -- 16. Escreva um comando que apresente os dados dos clientes que fizeram pedidos que
 --     foram atendidos pelo funcionário de código 1.
 
-SELECT c.*
+SELECT DISTINCT c.*
 
 FROM Cliente AS c
 
 INNER JOIN Pedido AS p
 ON p.Cod_Cli = c.Cod_Cli
 
-WHERE p.Cod_Func = 1
-
-GROUP BY c.Cod_Cli;
+WHERE p.Cod_Func = 1;
 
 -- OU
 
@@ -260,12 +371,38 @@ SELECT p.*
 
 FROM Pedido p
 
+LEFT JOIN Parcela pa
+ON pa.Num_Ped = p.Num_Ped
+
 WHERE
-  p.Cod_Sta IN ( SELECT Cod_Sta FROM StatusPedido WHERE Sta_Ped = 'Fechado' )
-  AND p.Num_Ped IN ( SELECT Num_Ped FROM Parcela WHERE Data_Venc = Data_Pgto );
+  -- XXX Pedidos não parcelados
+  pa.Num_Ped IS NULL;
+
+-- OU
+
+SELECT p.*
+
+FROM Pedido p
+
+WHERE
+  p.Num_Ped NOT IN ( SELECT Num_Ped FROM Parcela );
 
 -- 18. Escreva um comando que apresente os dados dos clientes que fizeram pedidos
 --     atendidos pelos funcionários que não têm dependentes.
+
+SELECT DISTINCT c.*
+
+FROM Cliente AS c
+
+INNER JOIN Pedido p
+ON p.Cod_Cli = c.Cod_Cli
+
+LEFT JOIN Dependente d
+ON d.Cod_Func = p.Cod_Func
+
+WHERE d.Cod_Func IS NULL;
+
+-- OU
 
 SELECT c.*
 
@@ -273,18 +410,9 @@ FROM Cliente AS c
 
 WHERE c.Cod_Cli IN (
 
-  SELECT p.Cod_Cli
-
-  FROM Pedido p
-
   -- XXX Funcionários SEM dependentes (NOT IN)
-  WHERE p.Cod_Func NOT IN (
-
+  SELECT p.Cod_Cli FROM Pedido p WHERE p.Cod_Func NOT IN (
     -- XXX Códigos de funcionários COM dependentes
-    SELECT Cod_Func
-
-    FROM Dependente
-
-  )
+    SELECT Cod_Func FROM Dependente)
 
 );
